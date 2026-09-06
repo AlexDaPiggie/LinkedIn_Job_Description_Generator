5bb41d73597b99696df288efcd1b61ef4fd4139a
Hoai Phong - Alex <alexhoaiphong@gmail.com>
2026-09-06 22:50:43 +0700

    update new domain

--- src/api/main.py
+++ src/api/main.py
@@ -221,8 +221,7 @@ def auth_change_username(request: ChangeUsernameRequest, authorization: str | No
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "https://linked-in-gen-ai.vercel.app",
+        "https://linkedingenai.tech",
     ],
     allow_credentials=False,
     allow_methods=["*"],
